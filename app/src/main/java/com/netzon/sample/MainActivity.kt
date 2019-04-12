ddea1802d27f048b392a44dbcbd8f326a0a0f91c
Quang Nguyen <minhquang211090@gmail.com>
2019-04-12 15:05:29 +0800

    Update the position of button to center

--- app/src/main/java/com/netzon/sample/MainActivity.kt
+++ app/src/main/java/com/netzon/sample/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.netzon.sample
 
-import android.graphics.Color
+import android.content.Intent
 import android.os.Bundle
+import android.text.Layout
 import androidx.appcompat.app.AppCompatActivity
 import com.facebook.litho.*
 import com.facebook.litho.annotations.LayoutSpec
 import com.facebook.litho.annotations.OnCreateLayout
 import com.facebook.litho.annotations.OnEvent
 import com.facebook.litho.annotations.Prop
+import com.facebook.litho.widget.EmptyComponent
 import com.facebook.litho.widget.Text
 import com.facebook.yoga.YogaAlign
 import com.facebook.yoga.YogaEdge
+import com.facebook.yoga.YogaJustify
+import com.netzon.sample.inviteUser.InviteUserActivity
 
 
 class MainActivity : AppCompatActivity() {
@@ -32,15 +36,23 @@ object CustomTextSpec {
     @OnCreateLayout
     fun onCreateLayout(c: ComponentContext,
                        @Prop title: String): Component =
-        Text.create(c)
+        Column.create(c)
             .alignSelf(YogaAlign.CENTER)
-            .text(title)
-            .textSizeDip(25F)
-            .touchExpansionDip(YogaEdge.ALL, 10f)
-            .build()
+            .justifyContent(YogaJustify.CENTER)
+            .child(
+                Text.create(c)
+                    .text(title)
+                    .textSizeDip(25F)
+                    .textAlignment(Layout.Alignment.ALIGN_CENTER)
+                    .touchExpansionDip(YogaEdge.ALL, 10f)
+                    .clickHandler(CustomText.onClick(c))
+                    .build()
+            ).build()
+
 
     @OnEvent(ClickEvent::class)
     fun onClick(c: ComponentContext) {
-
+        val intent = Intent(c.androidContext, InviteUserActivity::class.java)
+        c.androidContext.startActivity(intent)
     }
 }
