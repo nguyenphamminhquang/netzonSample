3e96f9684618b6c16caaf0beaf9975beeb40d12d
Quang Nguyen <minhquang211090@gmail.com>
2019-04-12 09:24:44 +0800

    setup sample text with Litho.

--- app/src/main/java/com/netzon/sample/MainActivity.kt
+++ app/src/main/java/com/netzon/sample/MainActivity.kt
@@ -2,11 +2,20 @@ package com.netzon.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.facebook.litho.ComponentContext
+import com.facebook.litho.LithoView
+import com.facebook.litho.widget.Text
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val context = ComponentContext(this)
+        val component = Text.create(context)
+            .text("Hello World")
+            .textSizeDip(25F)
+            .build()
+        setContentView(LithoView.create(context, component))
     }
 }
